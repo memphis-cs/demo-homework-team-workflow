4467d8014f62d24597f32461a3e8fdf7914abb69
Alice <sdflem+alice@gmail.com>
2019-01-31 22:02:34 +0000

    Completed Task 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home' 
+  get 'alice', to: 'pages#alice', as: 'alice'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
